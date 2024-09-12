2bc9595b93c65804abaa28aae8ed6049c71eae95
JohnnieEric <johnnie_eric@hotmail.com>
2024-09-12 21:46:39 +0800

    add pull to refresh and add more function

--- lib/article/article_list_content.dart
+++ lib/article/article_list_content.dart
@@ -1,3 +1,5 @@
+import 'package:easy_refresh/easy_refresh.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_learn_demo/article/article_api.dart';
 import 'package:flutter_learn_demo/article/webview_page.dart';
@@ -15,6 +17,7 @@ class ArticleListContent extends StatefulWidget {
 class _ArticleListContentState extends State<ArticleListContent> {
   List<ArticleData> _articleList = [];
   final ArticleApi _articleApi = ArticleApi();
+  bool _isRefreshing = false;
 
   @override
   void initState() {
@@ -24,15 +27,55 @@ class _ArticleListContentState extends State<ArticleListContent> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (_, index) => _buildItemByIndex(index),
-      itemCount: _articleList.length,
-      itemExtent: 80,
-    );
+    if (_isRefreshing) {
+      return const Center(
+        child: Wrap(
+          spacing: 10,
+          direction: Axis.vertical,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          children: [
+            CupertinoActivityIndicator(),
+            Text(
+              '数据加载中...',
+              style: TextStyle(color: Colors.grey),
+            )
+          ],
+        ),
+      );
+    }
+    return EasyRefresh(
+        header: const ClassicHeader(
+          dragText: '下拉刷新',
+          armedText: '释放刷新',
+          readyText: '开始加载',
+          processedText: '正在加载',
+        ),
+        onRefresh: _onRefresh,
+        onLoad: _onLoad,
+        child: ListView.builder(
+          itemBuilder: (_, index) => _buildItemByIndex(index),
+          itemCount: _articleList.length,
+          itemExtent: 80,
+        ));
+  }
+
+  void _onRefresh() async {
+    _articleList = await _articleApi.getArticlesList(0);
+    setState(() {});
+  }
+
+  void _onLoad() async {
+    int nextPage = _articleList.length ~/ 20;
+    List<ArticleData> list = await _articleApi.getArticlesList(nextPage);
+    _articleList = _articleList + list;
+    setState(() {});
   }
 
   void _getArticleListData() async {
+    _isRefreshing = true;
+    setState(() {});
     _articleList = await _articleApi.getArticlesList(0);
+    _isRefreshing = false;
     setState(() {});
   }
 
